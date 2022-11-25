a8ca841f69afa5b20c95c7903c29042e95fb994e
Lucas Oliveira <lucasrodriguesmano@gmail.com>
2022-11-25 02:23:15 -0300

    configuração do banco de dados e verificando rotas que ainda não estão funcionando

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,10 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  //log de todas as queries executadas no banco de dados (debugar).
+  log: ['query'],
+})
 
 async function bootstrap(){
   const fastify = Fastify({
@@ -6,6 +12,17 @@ async function bootstrap(){
   })
 
   //rotas
+  fastify.get('recycle/users', async () => {
+    const recycle = await prisma.user.findMany({
+      where:{
+        name: {
+          startsWith: 'L'
+        }
+      }
+    })
+
+    return { recycle }
+  })
 
   await fastify.listen({ port:3333 })
 
